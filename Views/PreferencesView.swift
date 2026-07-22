ca687db74592c01e064b73da15399a1f3bf8fd72
Ruyven <13271370+Ruyven@users.noreply.github.com>
2026-07-22 20:46:47 +1200

    fix: UI improvements

--- Views/PreferencesView.swift
+++ Views/PreferencesView.swift
@@ -10,32 +10,42 @@ import SwiftUI
 struct PreferencesView: View {
     @AppStorage(PrefKeys.musicScreensaverDelay) var musicDelay: Double = 60
     @AppStorage(PrefKeys.clockScreensaverDelay) var clockDelay: Double = 60
-
+    
+    var mainPadding: CGFloat = 4
+    
     var body: some View {
-        VStack {
+        Form {
             Section(header: Text("Screensaver Mode").font(.title)) {
                 Text("In screensaver mode, FrontRowTunes activates itself full-screen on inactivity.\nSet either delay to 0 to disable that screensaver mode condition.")
                     .font(.callout)
+                    .padding(.bottom, mainPadding)
                 
-                Form {
-                    VStack {
-                        Slider(value: $musicDelay, in: 0...300, step: 5) {
-                            Text("Music Playback Delay: \(Int(musicDelay))s")
-                        }
-                        Text("Time to wait while music is playing before entering screensaver mode.")
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                    
-                    VStack {
-                        Slider(value: $clockDelay, in: 0...300, step: 5) {
-                            Text("Clock Mode Delay: \(Int(clockDelay))s")
-                        }
-                        Text("Time to wait in clock mode before entering screensaver mode.")
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
+                HStack {
+                    Text("Music Playback Delay")
+                    Spacer()
+                    Text(musicDelay > 0 ? "\(Int(musicDelay))s" : "Disabled")
                 }
+                
+                Slider(value: $musicDelay, in: 0...300, step: 5)
+                
+                Text("Time to wait while music is playing before entering screensaver mode.")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .padding(.bottom, mainPadding)
+                
+                
+                HStack {
+                    Text("Full-Sized Clock Mode Delay")
+                    Spacer()
+                    Text(clockDelay > 0 ? "\(Int(clockDelay))s" : "Disabled")
+                }
+                
+                Slider(value: $clockDelay, in: 0...300, step: 5)
+                
+                Text("Time to wait in full-sized clock mode (option+T) before entering screensaver mode.")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                
             }
         }
         .padding()
